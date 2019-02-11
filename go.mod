2ec471747b51cf6d290a909405e56c651330af8c
Arturo Aparicio <arturoa@arturoa-mac.local>
2019-02-11 07:55:29 -0800

    Added a dependency

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/jfrog-aparicio/mixed-go
+
+require github.com/jfrog-aparicio/mixed-simple-mod v0.0.0-20190211154954-a52117b306ea
+
+require golang.org/x/net v0.0.0-20190110200230-915654e7eabc
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+        ellipse "github.com/jfrog-aparicio/mixed-simple-mod"
         "golang.org/x/net/context"
 )
 
-func main () {
-	fmt.Println ("Package Name: my package")
-        type Context = context.Context
-        type CancelFunc = context.CancelFunc
+func main() {
+ //Initalise the Init function with value of A,B
+ e := ellipse.Init{
+  9, 2,
+ }
+ //this will give answer as 0.9749960430435691
+ fmt.Println(e.GetEccentricity())
+ # Just to avoid go complaining that context is not being used 
+ type Context = context.Context
+ type CancelFunc = context.CancelFunc
 }
